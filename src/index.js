1e14a1e3a462c08e523eccff01be07cb59f01e39
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-07-17 03:40:53 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import './style.css';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+<h1>Hey! Ese are you there?</h1>,
+document.getElementById("root")
 );
